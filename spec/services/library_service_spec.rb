ffa661538932812f65cf8d6e85cb74b5ac276f73
Karan Mehta <karanm645@gmail.com>
2022-03-07 10:25:02 -0600

    Establish Service

--- spec/services/library_service_spec.rb
+++ spec/services/library_service_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe LibraryService do
 
   describe '#get_info' do 
     it 'returns library info' do 
-      library = service.get_info
+      library = service.get_info("denver,co")
       expect(library).to be_an Hash 
     end 
   end 
