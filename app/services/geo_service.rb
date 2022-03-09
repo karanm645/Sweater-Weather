a90b37b0dcae69a4cb930b108c8e7eab8d8eabaa
Karan Mehta <karanm645@gmail.com>
2022-03-08 19:05:35 -0600

    Establish connection (service)

--- app/services/geo_service.rb
+++ app/services/geo_service.rb
@@ -3,4 +3,13 @@ class GeoService
     response = Faraday.get("http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV["map_quest_key"]}&location=#{location}")
     JSON.parse(response.body, symbolize_names: true)
   end 
+  
+  def self.get_directions(start, finish)
+    conn = Faraday.new(url: "http://www.mapquestapi.com") do |faraday|
+      faraday.params["key"] = ENV["map_quest_key"]
+    end 
+    
+    response = conn.get("/directions/v2/route?from=#{start}&to=#{finish}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/geo_service_spec.rb
+++ spec/services/geo_service_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe GeoService do
       expect(geography).to be_an Hash 
     end 
   end 
+
+  describe '::get_directions' do 
+    it 'returns the directions' do 
+      directions = service.get_directions("denver,co", "pueblo,co")
+      expect(directions).to be_a Hash
+    end 
+  end 
 end 
